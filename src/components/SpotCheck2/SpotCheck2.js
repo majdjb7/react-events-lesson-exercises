e14d1affdda1cff5bc0e31839170075787a29897
majdjb7 <majdjd171@gmail.com>
2021-12-22 20:23:23 +0200

    Spotcheck 1 + 2 done

--- src/components/SpotCheck2/SpotCheck2.js
+++ src/components/SpotCheck2/SpotCheck2.js
@@ -2,13 +2,28 @@ import React, { Component } from 'react';
 
 export const Home = function () {
   // Should return some JSX with a div that has a class "home" and the text "Welcome to our page"
+  return <div>Welcome to our page</div>
+
 }
 
 export const About = function () {
   // Should return some JSX with a div that has a class "about" and the text "It's great here"
+  return <div>It is great here</div>
 }
 
 export class SpotCheck2 extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showHome: true
+    }
+  }
+
+  toggle = () => {
+    this.setState({
+      showHome: !this.state.showHome
+    })
+  }
 
   // Your SpotCheck2 should render the Home Component, the About Component, and a Toggle Button
   // Either the Home or About component should be rendered when you press the toggle button
@@ -18,7 +33,8 @@ export class SpotCheck2 extends Component {
   render() {
     return (
       <div >
-
+        <button onClick={this.toggle}>Toggle</button>
+        {this.state.showHome ? <Home/> : <About/>}
       </div>
     );
   }
